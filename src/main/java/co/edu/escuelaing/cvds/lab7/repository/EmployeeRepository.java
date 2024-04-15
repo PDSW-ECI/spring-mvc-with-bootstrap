c28e9d0fdc499a80e29f5d28ea1922d62f34e76a
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2024-04-15 12:39:53 -0500

    Agregue mas metodos en repository, service, controller

--- src/main/java/co/edu/escuelaing/cvds/lab7/repository/EmployeeRepository.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/repository/EmployeeRepository.java
@@ -1,5 +1,17 @@
 package co.edu.escuelaing.cvds.lab7.repository;
 
-public class EmployeeRepository {
-    
+import org.springframework.stereotype.Repository;
+
+import co.edu.escuelaing.cvds.lab7.model.Configuration;
+import co.edu.escuelaing.cvds.lab7.model.Employee;
+import co.edu.escuelaing.cvds.lab7.model.Configuration;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+    public List<Employee> findByIdEmployees(String employeeId);
 }
